e894481d4749a5c6a2f8c77838e6a7b3fb7daaf3
iDeaKz <94162009+iDeaKz@users.noreply.github.com>
2024-10-31 22:54:44 -0700

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from .adaptive_parallel import adaptive_parallel
+from .polymorphic_compute import polymorphic_compute
+from .logistic_regression_predict import logistic_regression_predict
+
+# You can define `__all__` to control what gets imported with `from predictiveforge import *`
+__all__ = ["adaptive_parallel", "polymorphic_compute", "logistic_regression_predict"]
